fba1d07df1d98dd696ef9265072f1ee3ec14d203
yanpei <1271882190@qq.com>
2017-09-21 15:28:45 +0800

    delete  compress img

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ var gulp = require("gulp"),                     //初始化gulp
     uglify = require("gulp-uglify"),            //压缩js文件
     concat = require("gulp-concat"),            //合并js
     jshint =require("gulp-jshint"),             //js校验 当js代码编译出现错误是会在命令行中 有错误提示
+    // imgMin = require("gulp-imagemin"),       //压缩图片   图片压缩率很小 深度压缩只适用于png图片
 
     rev = require("gulp-rev-append"),           //自动添加md5版本号
     clean =  require("gulp-clean"),             //删除之前编译过的文件
@@ -26,8 +27,8 @@ var path = {
     cssOut:"assets/styles/",
     jsIn:"dev/js/*.js",
     jsOut:"assets/scripts/",
-    imgIn:"dev/images/*",
-    imgOut:"assets/images/",
+    // imgIn:"dev/images/*",
+    // imgOut:"assets/images/",
     htmlIn:"dev/html/*.html",
     htmlOut:"assets/html/"
 };
@@ -39,7 +40,7 @@ gulp.task("styles",function () {
 
         .pipe(sass()) //编译
         .pipe(autoprefixer({
-            browsers: ['last 2 versions','Android >= 4.0'],
+            browsers: ['last 2 versions','Android >= 4.0'],//兼容浏览器最新两个版本 安卓4.0版本及以上版本
             cascade: false,//是否美化属性值 默认：true 像这样：
             //-webkit-transform: rotate(45deg);
             //        transform: rotate(45deg);
@@ -68,12 +69,13 @@ gulp.task("scripts",function () {
         .pipe(notify({message:"js task success"}))
 });
 
-//监听 images任务
-gulp.task("images",function () {
-    return gulp.src(path.imgIn)
-        .pipe(gulp.dest(path.imgOut))
-        .pipe(notify({message:"img task success"}))
-});
+// //监听 images任务
+// gulp.task("images",function () {
+//     return gulp.src(path.imgIn)
+//         .pipe(imgMin())
+//         .pipe(gulp.dest(path.imgOut))
+//         .pipe(notify({message:"img task success"}))
+// });
 
 //监听html任务 自动添加版本号
 gulp.task("revTest",function () {
@@ -87,6 +89,7 @@ gulp.task("revTest",function () {
 gulp.task("watch",function () {
     gulp.watch(path.cssIn,["styles"]);
     gulp.watch(path.jsIn,["scripts"]);
+    // gulp.watch(path.imgIn,["images"]);
     gulp.watch(path.htmlIn,["revTest"]);
 });
 
@@ -103,7 +106,7 @@ gulp.task('serve', function() {
 
 //创建默认的gulp任务
 gulp.task("default",function () {
-    gulp.start("styles",'watch','serve','scripts',"images",'revTest')
+    gulp.start("styles",'watch','serve','scripts','revTest')
 });
 
 
